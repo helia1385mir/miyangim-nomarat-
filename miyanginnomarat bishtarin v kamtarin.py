3b105d77397a6543f10a820fbc7bbd7af738a387
helia1385mir <88113615+helia1385mir@users.noreply.github.com>
2021-08-04 12:40:00 +0430

    Add files via upload

---
+++ miyanginnomarat bishtarin v kamtarin.py
@@ -0,0 +1,9 @@
+tedaddaneshamoozan=int(input())
+b=[]
+for i  in range(tedaddaneshamoozan) :
+    nomarat=float(input('nomre'))
+    b.append(nomarat)
+    
+print(sum(b)/tedaddaneshamoozan)
+print(max(b))
+print(min(b))
